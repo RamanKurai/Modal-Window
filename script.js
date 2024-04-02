9a94d3bbb2bfbdf0305501c953fcced6e66a2988
Raman Kurai <ramankurai27@gmail.com>
2024-04-02 20:56:19 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const OpenModal = function () {
+  console.log('Button clicked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', OpenModal);
+
+const CloseModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnCloseModal.addEventListener('click', CloseModal);
+overlay.addEventListener('click', CloseModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    CloseModal();
+  }
+});
